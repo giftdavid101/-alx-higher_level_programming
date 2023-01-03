5b21c12054f037c1d4856f62d223fe4661e3e4d4
giftdavid101 <giftdavid101@gmail.com>
2023-01-02 19:56:53 -0800

    10-check_cycle.c

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -19,5 +19,6 @@ struct listint_s *next;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 #endif

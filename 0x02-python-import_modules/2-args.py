f5ffa097f22a5381df975da7cdb1c25085433253
giftdavid101 <giftdavid101@gmail.com>
2023-01-05 20:30:30 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def args():
+    import sys
+
+    sys.argv.pop(0)
+    if len(sys.argv) != 1:
+        print("{} arguments:".format(len(sys.argv)))
+    else:
+        print("{} argument:".format(len(sys.argv)))
+    for i in range(len(sys.argv)):
+        print("{0}: {1}".format(i + 1, sys.argv[i]))
+
+if __name__ == "__main__":
+    args()
